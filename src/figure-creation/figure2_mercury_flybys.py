39153a72e7109a74fa322a34a85e8f45b5a4364f
Daragh Hollman <hollmandaragh@gmail.com>
2026-01-19 15:15:56 +0000

    Improved trajectory visualisation

--- src/figure-creation/figure2_mercury_flybys.py
+++ src/figure-creation/figure2_mercury_flybys.py
@@ -18,9 +18,11 @@ from astropy.table import QTable
 from astropy.time import Time, TimeDelta
 from hermpy.data import rotate_to_aberrated_coordinates
 from hermpy.net import ClientSPICE
+from hermpy.plotting import plot_magnetospheric_boundaries
 from hermpy.utils import Constants
 from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
-from matplotlib.patches import Circle, Rectangle
+from matplotlib.patches import Circle, Patch
+from matplotlib.ticker import AutoMinorLocator
 from planetary_coverage import MetaKernel
 from sunpy.time import TimeRange
 
@@ -42,6 +44,7 @@ LINE_PARAMS = {
         matplotlib.patheffects.Normal(),
     ],
 }
+LINE_STYLES = ["solid", "dotted", "dashed", "dashdot", (5, (10, 3))]
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
@@ -55,7 +58,7 @@ CLOSEST_APPROACH_TIMES: list[Time] = [
     Time("2022-06-23T09:44"),  # 2
     Time("2023-06-19T19:34"),  # 3
     Time("2024-09-04T21:48"),  # 4
-    Time("2024-12-01T14:23"),  # 5
+    # Time("2024-12-01T14:23"),  # 5 - outside of MESSENGER coverage
     Time("2025-01-08T05:59"),  # 6
 ]
 
@@ -67,7 +70,7 @@ PROBABILITY_MAP = load_probability_maps(
     Path(__file__).parent.parent.parent / "resources/region_probability_maps.nc"
 )
 
-FIG_OUTPUT = Path(__file__).parent.parent.parent / "figures/figure2_flybys.pdf"
+FIG_OUTPUT = Path(__file__).parent.parent.parent / "figures/figure2.pdf"
 
 
 def main():
@@ -140,15 +143,18 @@ def main():
 
     fig = plt.figure(figsize=(8, 8))
 
-    fig.text(0.02, 0.41, "Region Probability", fontsize="large", rotation=90)
+    fig.text(0.03, 0.41, "Region Probability", fontsize="large", rotation=90)
 
     # Some fancy nested grid logic to have panels with different ratios
     outer_grid = GridSpec(1, 2, width_ratios=[5, 2])
-    timeseries_grid = GridSpecFromSubplotSpec(6, 1, subplot_spec=outer_grid[0])
-    trajectory_grid = GridSpecFromSubplotSpec(3, 1, subplot_spec=outer_grid[1])
+    timeseries_grid = GridSpecFromSubplotSpec(5, 1, subplot_spec=outer_grid[0])
+    trajectory_grid = GridSpecFromSubplotSpec(5, 1, subplot_spec=outer_grid[1])
 
     timeseries_axes = []
     trajectory_axes = []
+
+    trajectory_handles = []
+    trajectory_labels = []
 
     for i in range(len(axis_data)):
         previous_ax = timeseries_axes[i - 1] if i != 0 else None
@@ -156,8 +162,8 @@ def main():
         ax = fig.add_subplot(timeseries_grid[i], sharex=previous_ax, sharey=previous_ax)
         timeseries_axes.append(ax)
 
-    for i in range(3):
-        ax = fig.add_subplot(trajectory_grid[i])
+        previous_ax = trajectory_axes[i - 1] if i != 0 else None
+        ax = fig.add_subplot(trajectory_grid[i], sharex=previous_ax, sharey=previous_ax)
         trajectory_axes.append(ax)
 
     timeseries_labels = "abcdef"
@@ -194,35 +200,48 @@ def main():
                 label=r"$\quad$95% CI",
             )
 
+        # Add vertical line at closest approach
+        minutes = data["Minutes Since Closest Approach"].value
+        ca_idx = np.argmin(np.abs(minutes))
+        ax.axvline(
+            minutes[ca_idx],
+            color="grey",
+            linewidth=1.5,
+            zorder=-5,
+        )
+
+        # Add vertical lines every 20 minutes
+        twenty_minute_mask = (np.abs(minutes) % 20) < (minutes[1] - minutes[0])
+        for t in minutes[twenty_minute_mask]:
+            ax.axvline(
+                t,
+                color="grey",
+                linewidth=0.6,
+                alpha=0.6,
+                zorder=-5,
+            )
+
         # Add panel label
         ax.text(0.01, 0.85, f"({timeseries_labels[i]})", transform=ax.transAxes)
 
         # Add an identifier for each trajectory to the flyby timeseries panel
-        position = (0.95 + 0.05, 0.8)
-        square = Rectangle(
-            position,
-            0.04,
-            0.16,
-            transform=ax.transAxes,
-            facecolor=wong_colours[i],
-            edgecolor="black",
-            clip_on=False,
-        )
-        ax.add_patch(square)
-
         ax.text(
-            position[0] + 0.005,
-            position[1] - 0.05,
-            f"Flyby {i + 1}",
-            ha="left",
-            va="top",
+            0.975,
+            0.5,
+            "Flyby 6" if i == 4 else f"Flyby {i + 1}",
+            ha="center",
+            va="center",
             rotation=-90,
             transform=ax.transAxes,
         )
 
+        # Change tick locations
+        ax.xaxis.set_ticks(np.arange(-120, 160, 40))
+        ax.xaxis.set_minor_locator(AutoMinorLocator())
+
         if ax == timeseries_axes[0]:
             ax.legend(
-                loc="upper center", bbox_to_anchor=(0.5, 1.8), ncol=3, framealpha=1
+                loc="upper center", bbox_to_anchor=(0.45, 1.8), ncol=3, framealpha=1
             )
 
         if ax == timeseries_axes[-1]:
@@ -234,34 +253,113 @@ def main():
         x = data["Positions"]["X MSM'"] / Constants.MERCURY_RADIUS.to(u.km)
         y = data["Positions"]["Y MSM'"] / Constants.MERCURY_RADIUS.to(u.km)
         z = data["Positions"]["Z MSM'"] / Constants.MERCURY_RADIUS.to(u.km)
+        rho = np.sqrt(y**2 + z**2)
 
-        trajectory_axes[0].plot(x, y, color=wong_colours[i], **LINE_PARAMS)
-        trajectory_axes[1].plot(x, z, color=wong_colours[i], **LINE_PARAMS)
-        trajectory_axes[2].plot(y, z, color=wong_colours[i], **LINE_PARAMS)
+        trajectory_axes[i].plot(x, rho, color="black", label="BepiColombo Trajectory")
 
-    trajectory_axes[0].set_xlabel(r"$X_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
-    trajectory_axes[0].set_ylabel(r"$Y_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
+        # For clarity, I'm adding dots along the trajectory every 10 minutes
+        # Find index of minimum distance
+        ca_idx = np.argmin(data["Distance"])
+        ax_scatter = trajectory_axes[i]
+        ax_scatter.scatter(
+            x[ca_idx],
+            np.sqrt(y[ca_idx] ** 2 + z[ca_idx] ** 2),
+            zorder=5,
+            color="black",
+            label="Closest Approach",
+        )
 
-    trajectory_axes[1].set_xlabel(r"$X_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
-    trajectory_axes[1].set_ylabel(r"$Z_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
+        minutes = data["Minutes Since Closest Approach"].value
+        twenty_minute_mask = (np.abs(minutes) % 20) < (minutes[1] - minutes[0])
 
-    trajectory_axes[2].set_xlabel(r"$Y_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
-    trajectory_axes[2].set_ylabel(r"$Z_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
+        # Compute angles from point-to-next-point
+        angles = np.degrees(
+            np.arctan2(np.diff(rho, prepend=rho[0]), np.diff(x, prepend=x[0]))
+        ).value
 
-    trajectory_labels = "ghi"
+        # Draw individual rotated triangles
+        _ = True
+        for idx in np.where(twenty_minute_mask)[0]:
+            ax_scatter.scatter(
+                x[idx],
+                rho[idx],
+                marker=(3, 0, angles[idx] - 90),
+                color="black",
+                s=20,
+                zorder=5,
+                label="20 Minute Intervals" if _ else "",
+            )
+
+            _ = False
+
+        if i == 0:
+            # We only need these once so can do it for only the first
+            ax_handles, ax_labels = trajectory_axes[0].get_legend_handles_labels()
+            trajectory_handles.extend(ax_handles)
+            trajectory_labels.extend(ax_labels)
+
+    residence_data = PROBABILITY_MAP["Minutes In Bin"] / 60  # Convert to hours
+    residence_mask = residence_data.values.T != 0
+    trajectory_ax_labels = "fghij"
     for i, ax in enumerate(trajectory_axes):
-        ax.text(0.02, 0.9, f"({trajectory_labels[i]})", transform=ax.transAxes)
+
+        if ax == trajectory_axes[-1]:
+            ax.set_xlabel(r"$X_{\rm MSM'} \quad \left[ R_{\rm M} \right]$")
+
+        ax.set_ylabel(r"$\rho \quad \left[ R_{\rm M} \right]$")
+        ax.text(0.02, 0.85, f"({trajectory_ax_labels[i]})", transform=ax.transAxes)
         ax.set_aspect("equal")
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(0, 10)
 
         # Add Mercury
         circle = Circle(
-            (0, 0), 1, edgecolor="black", facecolor="none", linewidth=2, zorder=5
+            (0, Constants.DIPOLE_OFFSET / Constants.MERCURY_RADIUS),
+            1,
+            edgecolor="grey",
+            facecolor="none",
+            linewidth=2,
+            zorder=-5,
+        )
+        ax.add_patch(circle)
+        circle = Circle(
+            (0, -1 * Constants.DIPOLE_OFFSET / Constants.MERCURY_RADIUS),
+            1,
+            edgecolor="grey",
+            facecolor="none",
+            linewidth=2,
+            zorder=-5,
         )
         ax.add_patch(circle)
 
-    plt.tight_layout(pad=2.5)
+        plot_magnetospheric_boundaries(ax, color="grey")
+
+        # Add MESSENGER outline
+        ax.contourf(
+            residence_data.coords["X MSM'"],
+            residence_data.coords["CYL MSM'"],
+            residence_mask,
+            levels=[0, 0.5, 1],
+            colors=["white", "lightgrey"],
+            zorder=-10,
+        )
+
+    # Contour doesn't apply in legends, so we need to fake it
+    proxy = Patch(facecolor="lightgrey", edgecolor="none")
+    trajectory_handles.append(proxy)
+    trajectory_labels.append("MESSENGER's Coverage")
+
+    fig.legend(
+        trajectory_handles,
+        trajectory_labels,
+        loc="lower center",
+        bbox_to_anchor=(0.85, 0.88),
+    )
+
+    for ax in trajectory_axes[:-1]:
+        ax.tick_params(labelbottom=False)
+
+    plt.subplots_adjust(right=0.99)
     plt.savefig(FIG_OUTPUT, format="pdf")
 
 
